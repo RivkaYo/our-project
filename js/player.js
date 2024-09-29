e5566313fa9f2432378ae9ea43988a52857b2ae8
rivka <rivkamiller@hilma.tech>
2024-09-29 14:28:18 +0300

    inital code

---
+++ js/player.js
@@ -0,0 +1,11 @@
+const chooseMeme = () => {
+    //import memes from photos.js
+    let chosenMeme = memes[Math.floor(Math.random * 9)];
+    return chosenMeme;
+};
+
+//need to set img crs at player.html to chooseMeme
+
+
+//save player1's answer (value) to a vareable.
+

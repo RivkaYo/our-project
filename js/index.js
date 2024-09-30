4644a4add40f4a1acd3c5ebea4ca7dc4f2a316ee
rivka <rivkamiller@hilma.tech>
2024-09-30 14:42:01 +0300

    local storage and var organized

--- js/index.js
+++ js/index.js
@@ -3,44 +3,82 @@ localStorage.clear();
 if(!localStorage.getItem("users"))
   localStorage.setItem("users",JSON.stringify([]))
 
-console.log(memes)
+let registerForm = {username:"",password:""};
 
-  let registerForm={username:"",password:""}
+function handleRegisterChange(){
+  registerForm={username:document.getElementById("register-username").value,password:document.getElementById("register-password").value}
+}
 
-  function handleRegisterChange(){
-     registerForm={username:document.getElementById("register-username").value,password:document.getElementById("register-password").value}
+function onRegisterSubmit(){
+  let users=JSON.parse(localStorage.getItem("users"))
+  localStorage.setItem("users",JSON.stringify([...users,registerForm]))
+}
+
+let logForm = {};
+function handleLogChange(){
+  logForm = {
+    username: document.getElementById("username").value,
+    password: document.getElementById("password").value,
+    isJudge: false,
+    score: 0
   }
+  console.log(logForm);
+}
 
-  function onRegisterSubmit(){
-    let users=JSON.parse(localStorage.getItem("users"))
-    localStorage.setItem("users",JSON.stringify([...users,registerForm]))
-  }
-
-  let logForm={username:"",password:""}
-
-  function handleLogChange(){
-  logForm={username:document.getElementById("username").value,password:document.getElementById("password").value}
-  }
-
-  function onLogSubmit(){
-    let users=JSON.parse(localStorage.getItem("users"))
-    let loggeduser=users.find(check);
-    for(let j=0;j<users.length;j++) {
-      if (users.find(check)) {
-        document.getElementById("demo").innerHTML+=loggeduser.username;
-      }
+function onLogSubmit(){
+  let users=JSON.parse(localStorage.getItem("users"))
+  let loggeduser=users.find(check);
+  for(let j=0;j<users.length;j++) {
+    if (users.find(check)) {
+      document.getElementById("demo").innerHTML+=loggeduser.username;
     }
   }
+  console.log(handleLogChange());
+}
 
 function check(user) {
   return user.username===logForm.username && logForm.password===user.password ;
 }
 
-  /*function readUsername() {
-    
-    var x = localStorage.getItem("username");
-    document.getElementById("demo").innerHTML = x;
-  } */
 
-//let p = document.getElementById("RegisterSubmit").addEventListener("click", readUsername);
-//.innerHTML
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  users = [{email: ~~, password: ~~}, {}]
+
+
+//  for every object in users does logiined.email exist ?
+//  if yes, object.password === looggedinned.passrowd ? LOGIN : WRONG PASSWORD

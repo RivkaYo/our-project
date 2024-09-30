b0ce85f01d4f2650ed0a66bda6c02d58102418c8
rivka <rivkamiller@hilma.tech>
2024-09-30 21:28:27 +0300

    the choose judge button works

--- js/choose-judge.js
+++ js/choose-judge.js
@@ -22,18 +22,33 @@ changeToPlayerUsername("player3", 2);
 
 const makeJudge = event => {
     console.log(event.target);
-    
+    let playerNum = event.target.id;
+    if (playerNum === "player1") {
+        let player = JSON.parse(localStorage.getItem("loggedUsers"))[0];
+        player.isJudge = true;
+        console.log(player);
+    } else if (playerNum === "player2") {
+        let player = JSON.parse(localStorage.getItem("loggedUsers"))[1];
+        player.isJudge = true;
+        console.log(player);
+    } else if (playerNum === "player3") {
+        let player = JSON.parse(localStorage.getItem("loggedUsers"))[2];
+        player.isJudge = true;
+        console.log(player);
+    }
 }
 
-console.log('getElementsByClassName: ', document.getElementsByClassName("playerButton"));
-// document.getElementsByClassName("playerButton").forEach(element => {
+//console.log('getElementsByClassName: ', document.getElementsByClassName("playerButton"));
+//document.getElementsByClassName("playerButton").forEach(element => {
 //     element.addEventListener("click", makeJudge);
 // });
 
-const playerElements = document.getElementsByClassName("playerButton")
-for(let i = 0; i < playerElements.length; i++) {
-    playerElements[i].addEventListener("click", makeJudge);
-}
+//const playerElements = document.getElementsByClassName("playerButton")
+//for(let i = 0; i < playerElements.length; i++) {
+//    playerElements[i].addEventListener("click", makeJudge);
+//}
 
-
+document.getElementById("player1").addEventListener('click', makeJudge);
+document.getElementById("player2").addEventListener('click', makeJudge);
+document.getElementById("player3").addEventListener('click', makeJudge);
 

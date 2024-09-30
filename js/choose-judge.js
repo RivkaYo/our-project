07c2211edbf34648c00dd4c41b3e826da9a58ade
rivka <rivkamiller@hilma.tech>
2024-09-30 16:11:27 +0300

    milti looged on possible and styling

--- js/choose-judge.js
+++ js/choose-judge.js
@@ -1,5 +1,4 @@
 
-
 const changeOnHover = () => {
     width = 200;
     height = 200;
@@ -8,3 +7,9 @@ const changeOnHover = () => {
 document.getElementsByClassName("playerButton").addEventListener("hover", changeOnHover);
 
 document.getElementsByClassName("playerButton").addEventListener("click", setJudge);
+
+//set player1 to player1's username
+var player1Username = localStorage.getItem("loggedUsers[0]");
+document.getElementsByClassName("player1").innerHTML(player1Username);
+
+

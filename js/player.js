4d820d0ebf2e482ad6db9b84e08dd343d5487aff
rivka <rivkamiller@hilma.tech>
2024-09-30 09:13:49 +0300

    add players as list of odjects and try insert random meme

--- js/player.js
+++ js/player.js
@@ -1,11 +1,42 @@
+import { memes } from './photos';
+
+console.log("hi boaz");
+
+console.log ("memesList = ", memes);
 const chooseMeme = () => {
     //import memes from photos.js
-    let chosenMeme = memes[Math.floor(Math.random * 9)];
-    return chosenMeme;
+    let randomMeme = memes[Math.floor(Math.random * 9)];
 };
 
-//need to set img crs at player.html to chooseMeme
+let meme = document.getElementById('meme');
 
+chooseMeme();
+
+//players as list of odjects
+let players = [
+    player1 = {name: document.getElementById("demo").innerHTML=loggeduser.username,
+    score: 0,
+    isJudge: false
+    },
+    player2 = {name: document.getElementById("demo").innerHTML=loggeduser.username,
+    score: 0,
+    isJudge: false
+    },
+    player3 = {name: document.getElementById("demo").innerHTML=loggeduser.username,
+    score: 0,
+    isJudge: false
+    },
+];
+
+//need to set img crs at player.html to chooseMeme
+Img.src = chooseMeme();
 
 //save player1's answer (value) to a vareable.
 
+
+document.getElementsByTagName(submit).addEventListener("click", addAnswer);
+
+const addAnswer = function() {
+    let player1Answer = document.getElementById("playerAnswer").value;
+    console.log(player1Answer);
+}
